0b7b28b96bcbfd2d51e7c93cce03545b8e88c4c1
Kareem <kareem@datum.org>
2018-10-08 18:36:00 +0800

    Refactor crypto.js and related code.
    - Add checks to call startCollectors if windows is undefined.
    - Remove adata elements and all empty configs from crypto.js.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require("babel-polyfill");
+const sjcl = require("sjcl");
 
-var Datum = require('./lib/datum');
+var Datum = require("./lib/datum");
 
-if (typeof window !== 'undefined' && typeof window.Datum === 'undefined') {
+if (typeof window !== "undefined"&& typeof window.Datum === "undefined")
+{
     window.Datum = Datum;
+}else{
+    /**
+     * Enhance entropy in case SDK is loaded on none browser environment
+     */
+    sjcl.random.startCollectors();
 }
-
 module.exports = Datum;
